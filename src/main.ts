1aa7b8ac7df2abd3623057fc943c3e9c713a76b8
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-12-02 18:19:26 +0100

    Explicitely specify host and port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
     AppModule,
     {
       transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3000,
+      },
     },
   );
   await app.listen();
