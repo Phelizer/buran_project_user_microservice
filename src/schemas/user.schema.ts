34c376e2ec63d909f68a6e4a8b27f7073c515826
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-12-16 20:11:23 +0100

    Amend user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,9 +1,22 @@
 import mongoose from 'mongoose';
+import { ROLES } from '../models/role.model';
+import { User } from 'src/models/user.model';
 
-export const UserSchema = new mongoose.Schema({
-  login: String,
+export const UserSchema = new mongoose.Schema<User>({
   id: String,
-  roles: [String],
+  first_name: String,
+  last_name: String,
+  email: String,
+  registration_date: Date,
+  age: Number,
+  sex: {
+    type: String,
+    enum: ['male', 'female'],
+  },
+  roles: {
+    type: [String],
+    enum: Object.values(ROLES),
+  },
 });
 
 export const USER_MODEL_NAME = 'User';
