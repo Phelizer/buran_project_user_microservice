f0920aeb436d2591ed8fc24d5ef002c64e82b5dc
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-12-16 20:32:24 +0100

    Amend seedScript to match new user schema and generate realistic data

--- seedScript.js
+++ seedScript.js
@@ -1,11 +1,103 @@
 var collection = db.users;
 
-function generateLogin() {
-  return (Math.random() + 1).toString(36).substring(7);
+const NAMES = [
+  'James',
+  'John',
+  'Robert',
+  'Michael',
+  'William',
+  'David',
+  'Richard',
+  'Joseph',
+  'Charles',
+  'Thomas',
+  'Christopher',
+  'Daniel',
+  'Matthew',
+  'Anthony',
+  'Donald',
+  'Mark',
+  'Paul',
+  'Steven',
+  'Andrew',
+  'Brian',
+];
+
+const SURNAMES = [
+  'Smith',
+  'Johnson',
+  'Williams',
+  'Brown',
+  'Jones',
+  'Miller',
+  'Davis',
+  'García',
+  'Rodriguez',
+  'Martinez',
+  'Hernandez',
+  'Lopez',
+  'Gonzalez',
+  'Wilson',
+  'Anderson',
+  'Thomas',
+  'Taylor',
+  'Moore',
+  'Jackson',
+  'White',
+];
+
+function gaussianRandom(mean = 0, stdev = 1) {
+  const u = 1 - Math.random();
+  const v = Math.random();
+  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  return z * stdev + mean;
 }
 
-collection.insertMany([
-  { login: generateLogin(), id: '1', roles: ['user'] },
-  { login: generateLogin(), id: '2', roles: ['user'] },
-  { login: generateLogin(), id: '3', roles: ['user'] },
-]);
+const generateAge = () => gaussianRandom(25);
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomElem(arr) {
+  const index = getRandomArbitrary(0, arr.length - 1);
+  return arr[index];
+}
+
+const getRandomName = () => getRandomElem(NAMES);
+const getRandomSurname = () => getRandomElem(SURNAMES);
+
+const EMAIL_HOSTS = [
+  'gmail.com',
+  'yahoo.com',
+  'outlook.com',
+  'hotmail.com',
+  'example.com',
+];
+
+const getRandomEmailHost = () => getRandomElem(EMAIL_HOSTS);
+
+function generateEmail() {
+  return `${getRandomName}.${getRandomSurname}${getRandomArbitrary(
+    100,
+    999,
+  )}@${getRandomEmailHost}`;
+}
+
+const mockUser = () => ({
+  id: getRandomArbitrary(10000000000, 99999999999).toString(),
+  first_name: getRandomName(),
+  last_name: getRandomSurname(),
+  email: generateEmail(),
+  registration_date: new Date(),
+  age: generateAge(),
+  sex: Math.random() > 0.2 ? 'male' : 'female',
+  roles: ['user'],
+});
+
+const users = [];
+for (let i = 0; i < 100; i++) {
+  users.push(mockUser());
+}
+
+collection.insertMany(users);
