402d6ed1ca1e41fb89db9808d2a6eb7dda10e768
Oleksandr Chornyi <chornyi.oleksandr@lll.kpi.ua>
2023-12-19 00:45:57 +0100

    Create sample test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { USER_MODEL_NAME } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { User } from './models/user.model';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let appService: AppService;
+  let userModel: Model<User>;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: getModelToken(USER_MODEL_NAME),
+          useValue: { findOne: () => null },
+        },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appService = app.get(AppService);
+    userModel = app.get(getModelToken(USER_MODEL_NAME));
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should return user', async () => {
+    const userID = 'sampleID';
+    const mockedUser: User = {
+      age: 21,
+      email: 'example@gmail.com',
+      first_name: 'John',
+      last_name: 'White',
+      id: userID,
+      registration_date: new Date(),
+      roles: ['USER'],
+      sex: 'male',
+    };
+
+    jest
+      .spyOn(userModel, 'findOne')
+      .mockReturnValueOnce(new Promise((res) => res(mockedUser)) as any);
+
+    const user = await appService.getUser(userID);
+
+    expect(user).toBeDefined();
+    expect(user).toEqual(mockedUser);
+    expect(userModel.findOne).toHaveBeenCalledWith({ id: userID });
   });
 });
